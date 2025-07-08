db761b7a6a8cd4c3b8f85430d56ca968d394b27c
abdullah <elgendyabdala@gmail.com>
2025-07-08 15:52:27 +0300

    add method removeOutdatedBooks remove books passed specific number of years

--- src/main/java/BookStore.java
+++ src/main/java/BookStore.java
@@ -7,6 +7,9 @@ public class BookStore {
     private  Map<String, Book> inventory = new HashMap<>();
 
     public void addBook(Book book) {
+        if (book == null) {
+            throw new IllegalArgumentException("Book cannot be null.");
+        }
         if (inventory.containsKey(book.getISBN())) {
             throw new IllegalArgumentException("Book with ISBN " + book.getISBN() + " alredy exists.");
         }
@@ -14,6 +17,18 @@ public class BookStore {
         inventory.put(book.getISBN(), book);
     }
 
+    public List<Book> removeOutdatedBooks(int currentYear, int numberOfYears) {
+        List<Book> outdated = new ArrayList<>();
+        for (Book book : inventory.values()) {
+            if (currentYear - book.getYear() > numberOfYears) {
+                outdated.add(book);
+            }
+        }
+        for (Book book : outdated) {
+            inventory.remove(book.getISBN());
+        }
+        return outdated;
+    }
 
 
     public double buyBook(String ISBN, int quantity, String email, String address) throws Exception {
